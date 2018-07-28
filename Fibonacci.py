8d392f7f0f890d92f3dcde6aa7a2eec962b42bc6
William Li <37913455+WilliamLiii@users.noreply.github.com>
2018-07-27 23:01:13 -0500

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,19 @@
+def Fibonnaci(n):
+    if n == 0:
+        result = None
+    elif n == 1:
+        result = [1]
+    elif n == 2:
+        result = [1,1]
+    elif n > 2:
+        result = [1,1]
+        count_number = 2
+        while n > 2:
+            result.append(result[-1]+result[-2])
+            count_number += 1
+            if count_number == n:
+                break
+    print(result)
+
+num = int(input('Please type a number: '))
+Fibonnaci(num)
